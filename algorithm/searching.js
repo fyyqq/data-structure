f25454f18e55c32d505aed3a802ec3138f8b8ff6
= <=>
2023-09-22 17:35:18 +0800

    create binary search function on searching.js

--- algorithm/searching.js
+++ algorithm/searching.js
@@ -13,8 +13,32 @@ function linearSearch(arrays, target) {
     return { message: `We don't found ${target}` };
 }
 
-const numbers = [2, 7, 3, 5, 1, 15, 8, 4, 10, 14];
-const searchNumber = 2;
+const linearNumbers = [2, 7, 3, 5, 1, 15, 8, 4, 10, 14];
+const linearSearchNumber = 2;
 
-const linear = linearSearch(numbers, searchNumber);
-console.log(linear);
+const ls = linearSearch(linearNumbers, linearSearchNumber);
+console.log(ls);
+
+
+function binarySearch(arrays, target) {
+    let right = arrays.length - 1; // 9
+    let left = 0;
+    
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (arrays[mid] === target) {
+            return { index: mid, number: arrays[mid] };
+        } else if (arrays[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return { message: `We don't found ${target}` };
+}
+
+const binaryNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const binarySearchNumber = 8;
+
+const bl = binarySearch(binaryNumbers, binarySearchNumber);
+console.log(bl);
